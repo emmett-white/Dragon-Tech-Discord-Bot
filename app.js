442610f7b46ecec5e62380013ab6ce828bcdf37f
Emmett <62869287+emmett-white@users.noreply.github.com>
2020-04-29 19:02:53 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,75 @@
-require('dotenv').config();
+'use strict';
 
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+require('dotenv').config()
 
-const TOKEN = process.env.TOKEN;
+const guild = 						require('guild')
+const { Client, MessageEmbed } = 	require('discord.js')
 
-bot.on('ready', () => {
-	console.info(`Logged in as ${bot.user.tag}!`);
-});
+const client = new Client()
 
-bot.on('message', msg => {
+const TOKEN = process.env.TOKEN
+
+client.on('ready', () => {
+	console.info(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', async msg => {
 	if (msg.content === '$callbot') {
 		msg.channel.send(
-			'**Beep boop...**\n\I`m here, how can I help you?\n\For help type $help.'
+			'**Beep boop...**\n\Hello, I`m here, how can I help you?\n\For help type $help.'
 		)
+
+		msg.react('✋') // ✋ = :raised_hand:
 	}
 
 	else if (msg.content === '$help') {
-    	msg.channel.send('**Commands**\n- $help\n- $votekick.');
-  	}
+		const embed = new MessageEmbed()
 
-  	else if (msg.content.startsWith('$votekick')) {
-  		if (msg.mentions.users.size) {
-    		const taggedUser = msg.mentions.users.first();
+		.setTitle('Dragon-Tech Support')
+		.setColor(0xd4c222)
+		.setDescription('**Commands**:\n- $help\n- $votekick\n- $staff\n- $ping\n- $avatar');
 
-    		msg.channel.send(`Vote kick for: ${taggedUser.username}`);
-  		} else msg.reply('Please tag a valid user!');
+	    msg.channel.send(embed);
 	}
-});
 
-bot.login(TOKEN);
+  	else if (msg.content === '$votekick') {
+  		if (msg.mentions.users.size) {
+    		const taggedUser = msg.mentions.users.first()
+
+    		msg.channel.send(`Vote kick for: ${taggedUser.username}`)
+  		} else msg.reply('Please tag a valid user!')
+	}
+
+	else if (msg.content === '$staff') {
+		let
+			// Role ID's
+			techSupport = "696089336978079805",
+			moderator = "696089600560726137",
+			hacker = "696088492601507912",
+			webDev = "704404302244085851",
+			admin = "696083121107632148",
+			owner = "696083537698226186",
+
+			// Members with that roles
+			membersWithTechSupp = msg.guild.roles.cache.get(techSupport).members,
+			membersWithMod = msg.guild.roles.cache.get(moderator).members,
+			membersWithHacker = msg.guild.roles.cache.get(hacker).members,
+			membersWithWebDev = msg.guild.roles.cache.get(webDev).members,
+			membersWithAdmin = msg.guild.roles.cache.get(admin).members,
+			membersWithOwner = msg.guild.roles.cache.get(owner).members
+
+		msg.channel.send(`** STAFF TEAM INFORMATION **\n\nOwners: ${membersWithOwner.size} members with that role.\nAdministrators: ${membersWithAdmin.size} members with that role.\nModerators: ${membersWithMod.size} members with that role.\nTech Supports: ${membersWithTechSupp.size} members with that role.\nWeb Developers: ${membersWithWebDev.size} members with that role.\nHackers: ${membersWithHacker.size} members with that role.`)
+	}
+
+	// Check ping
+	else if (msg.content === '$ping') {
+		msg.channel.send("Your ping is `"
+			+ `${(Date.now() - msg.createdTimestamp)}` + " ms`")
+	}
+
+	else if (msg.content === '$avatar') {
+		msg.reply(msg.author.displayAvatarURL());
+	}
+})
+
+client.login(TOKEN)
